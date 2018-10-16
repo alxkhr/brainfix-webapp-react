b2cbd98326ab9d14aaa5cf9a83aab47f85729231
Alexander Kehr <alex@alexanderkehr.de>
2018-10-16 10:55:12 +0200

    added basic manifest.json

--- src/state/note/reducers.ts
+++ src/state/note/reducers.ts
@@ -1,22 +1,10 @@
 import { AnyAction } from 'redux';
 
 import Note from '../../shared/model/note';
-import { GET_NOTES, RECEIVED_NOTES, SET_NOTES } from './actions';
+import { RECEIVED_NOTES } from './actions';
 import NoteState from './model/note-state';
 
-function getNotes(state: NoteState): NoteState {
-  return { ...state };
-}
-
 function receivedNotes(state: NoteState, notes: Note[]): NoteState {
-  return {
-    ...state,
-    // notes: mergeNotes(state.notes, notes),
-    notes,
-  };
-}
-
-function setNotes(state: NoteState, notes: Note[]): NoteState {
   return {
     ...state,
     // notes: mergeNotes(state.notes, notes),
@@ -42,12 +30,8 @@ const defaultNoteState: NoteState = {
 
 export default function(state: NoteState = defaultNoteState, action: AnyAction): NoteState {
   switch (action.type) {
-    case GET_NOTES:
-      return getNotes(state);
     case RECEIVED_NOTES:
       return receivedNotes(state, action.notes);
-    case SET_NOTES:
-      return setNotes(state, action.notes);
     default:
       return state;
   }
